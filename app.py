87334201cd1e4d92eba170f18d833ef5d6c6bd34
BertrandOscarSaputra <bertrandosc@gmail.com>
2025-05-04 21:59:59 +0800

    Bootstrap Update

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from tensorflow.keras.applications.inception_v3 import preprocess_input
 import numpy as np
 import os
 
+
 app = Flask(__name__)
 model = load_model("./Model/inceptionv3_model.h5", compile=False)
 
@@ -17,27 +18,24 @@ def index():
     if request.method == "POST":
         img_file = request.files["image"]
         if img_file:
-            img_path = os.path.join("static", img_file.filename)
+            img_path = os.path.join("static/uploads", img_file.filename)
             img_file.save(img_path)
 
-            # Load and preprocess the image
-            img = image.load_img(img_path, target_size=(224, 224))  # 224x224 for your model
+            img = image.load_img(img_path, target_size=(224, 224))  
             img_array = image.img_to_array(img)
             img_array = np.expand_dims(img_array, axis=0)
-            img_array = preprocess_input(img_array)  # Normalize the image
+            img_array = preprocess_input(img_array)  
 
-            # Make prediction
             preds = model.predict(img_array)
 
-            # Convert prediction to label and confidence
             class_names = ["benign", "malignant"]
 
             confidence = round(float(preds[0][0]) * 100, 2)
 
             if preds[0][0] > 0.5:
-                prediction = class_names[1]  # malignant
+                prediction = class_names[1]  
             else:
-                prediction = class_names[0]  # benign
+                prediction = class_names[0]  
                 confidence = 100 - confidence 
 
     return render_template("index.html", prediction=prediction, confidence=confidence, img_path=img_path)
